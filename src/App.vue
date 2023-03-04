f5cc64fd12f60f3f3bc60a9b1a723f54bd0d2c2e
MisaelMa <amisael.amir.misael@gmail.com>
2023-03-04 10:18:49 -0500

    f

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,32 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+const packages = [
+  {
+    name:'@cfdi/xml',
+    icon: 'xml'
+  },
+  {
+    name:'@cfdi/csd',
+    icon: 'csd'
+  },
+ 
+]
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+<div class="grid grid-cols-4 gap-4">
+  <div v-for="(lib,i) of packages"  :key="i" >
+    {{lib}}
+    <a href="#" :key="i" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+      {{lib.name}}
+    </h5>
+    <p class="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+</a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+</div>
+
 </template>
 
 <style scoped>
